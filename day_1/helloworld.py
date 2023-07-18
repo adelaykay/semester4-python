5002ac11f0851f274299529307f14230b90ec89c
adelaykay <adelekeolasope@gmail.com>
2023-07-18 02:26:34 +0100

    operators and data types

---
+++ day_1/helloworld.py
@@ -0,0 +1,28 @@
+# Arithmetic Operations
+print(3+4) # Addition
+
+print(3-4) # Subtraction
+
+print(3*4) # Multiplication
+
+print(3%4) # Modulus
+
+print(3/4) # Division
+
+print(3**4) # Exponentiation
+
+print(3//4) # Floor / Integer Division
+
+# Data Types
+print('Adeleke')
+print('Olasope')
+print('Nigeria')
+print('I am enjoying 30 days of python')
+print(type(10))
+print(type(9.8))
+print(type(3.14))
+print(type(4-4j))
+print(type(['Adeleke','Olaolu','Olasope']))
+print(type('Adeleke'))
+print(type('Olasope'))
+print(type('Canada'))
