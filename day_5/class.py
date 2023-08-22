911986bebd250873d36b3c8fdcd01f5c3f5ac753
adelaykay <adelekeolasope@gmail.com>
2023-08-22 14:26:01 +0100

    no changes

---
+++ day_5/class.py
@@ -0,0 +1,15 @@
+
+fruits=['apple', 'orange', 'mango', 'apple', 'orange', 'mango']
+# fruits.append('grape')
+# fruits.insert(2, 'grape')
+# fruits.remove('banana')
+# fruits.pop()
+# del fruits[0:2]
+# fruits.clear()
+# fruits.reverse()
+# fruits.sort()
+copy_of_fruits = fruits.copy()
+fruits2 = [*fruits]
+
+# print(fruits, copy_of_fruits == fruits)
+print(fruits == fruits2)
