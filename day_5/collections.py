9afddc648c86e339e790c5bfae06e3700b46ff78
adelaykay <adelekeolasope@gmail.com>
2023-08-22 14:28:15 +0100

    no changes

--- day_5/collections.py
+++ day_5/collections.py
@@ -65,3 +65,18 @@ print(f'15. {"Google" in it_companies}')
 it_companies.sort()
 print(f'16. IT companies {it_companies}')
 
+
+fruits=['apple', 'orange', 'mango', 'apple', 'orange', 'mango']
+# fruits.append('grape')
+# fruits.insert(2, 'grape')
+# fruits.remove('banana')
+# fruits.pop()
+# del fruits[0:2]
+# fruits.clear()
+# fruits.reverse()
+# fruits.sort()
+copy_of_fruits = fruits.copy()
+fruits2 = [*fruits]
+
+# print(fruits, copy_of_fruits == fruits)
+print(fruits == fruits2)
